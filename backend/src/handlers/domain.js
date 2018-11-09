e98cb2da76949d4579770eea90b0aac84e876bcc
Daniil Beltyukov <beltyukovdanil@gmail.com>
2018-11-10 02:07:27 +0300

    Domain handler. Closes #6

---
+++ backend/src/handlers/domain.js
@@ -0,0 +1,50 @@
+const uuid = require('uuid/v4');
+const {
+  RedisQueue
+} = require('task-queue');
+
+// Config for Queue and RedisClient.
+const QUEUE_WHOIS_NAME = 'queue:whois';
+// Domain validation regexp
+const DOMAIN_REGEX = /[a-zA-Z0-9-]+/;
+
+const domainHandler = async (req, res) => {
+  const {
+    domain
+  } = req.params;
+  const {
+    redisClient
+  } = req;
+
+  try {
+    // Check if valid domain
+    if (!domain.test(DOMAIN_REGEX)) {
+      res.status(400).send('Bad Request');
+    }
+    // Generate unique id for request
+    const randId = uuid();
+    // Create a queue where we put tasks
+    const queueFormer = new RedisQueue({
+      queueName: QUEUE_WHOIS_NAME,
+      redisClient: redisClient
+    });
+
+    queueFormer.on('error', (err) => {
+      console.error(err);
+    });
+
+    // Put task
+    await queueFormer.put({
+      type: 'whois',
+      args: [ domain ],
+      id: randId
+    });
+
+    res.status(200).send(randId);
+  } catch (e) {
+    console.error(e.error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+module.exports = domainHandler;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,25 @@
 const process = require('process');
 const express = require('express');
+const Redis = require('ioredis');
 const rootHandler = require('./handlers/root');
+const domainHandler = require('./handlers/domain');
 
 const hostname = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+// Redis connection options in one url
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
 const app = express();
 
+// Create a connection
+const redisClient = new Redis(REDIS_URL);
+
+// Let handlers reuse redis connection
+app.use((req, res, next) => {
+  req.redisClient = redisClient;
+  return next();
+});
 if (process.env.NODE_ENV === 'dev') {
   const path = require('path');
 
@@ -14,6 +27,7 @@ if (process.env.NODE_ENV === 'dev') {
 } else {
   app.get('/', rootHandler);
 }
+app.get('/domain/:domain', domainHandler);
 app.listen(port, hostname, () => {
   console.log(`Server running at ${hostname}:${port}/`);
 });
