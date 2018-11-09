3da0ae0b7388b40028ac8bd4ca9a29fe2f76da13
Daniil Beltyukov <beltyukovdanil@gmail.com>
2018-11-10 00:46:38 +0300

    Serve frontend. Closes #5

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', rootHandler);
+if (process.env.NODE_ENV === 'dev') {
+  const path = require('path');
 
+  app.use(express.static(path.join(__dirname, '../../frontend/src/')));
+} else {
+  app.get('/', rootHandler);
+}
 app.listen(port, hostname, () => {
   console.log(`Server running at ${hostname}:${port}/`);
 });
