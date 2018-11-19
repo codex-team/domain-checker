f174ed5f9ac9ac0d07fec1f4c4b6b1e81f525b33
Nikita Melnikov <nikmel2803@gmail.com>
2018-11-19 19:43:56 +0300

    debounce update

--- frontend/src/js/utils/debounce.js
+++ frontend/src/js/utils/debounce.js
@@ -9,10 +9,9 @@ export default function debounce(func, delay) {
   let inDebounce;
 
   return function () {
-    const context = this;
     const args = arguments;
 
     clearTimeout(inDebounce);
-    inDebounce = setTimeout(() => func.apply(context, args), delay);
+    inDebounce = setTimeout(() => func(...args), delay);
   };
 };
