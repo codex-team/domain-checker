080ddd6b0ab54d221b5513c48d067c0508f0d799
Nikita Melnikov <nikmel2803@gmail.com>
2018-11-19 19:08:39 +0300

    webpack output change

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'src/dist')
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
