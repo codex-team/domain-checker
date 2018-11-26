9dcb6615114b3a0f1e636b2a8d4129fb14ceb6fa
Daniil Beltyukov <beltyukovdanil@gmail.com>
2018-11-26 13:08:50 +0300

    Fix api/checkDomain route could't load registry

--- backend/src/helpers/registry.js
+++ backend/src/helpers/registry.js
@@ -1,10 +1,11 @@
-const { Registry } = require('../workers/lib/registry');
+// const { Registry } = require('../workers/lib/registry');
+const { Worker } = require('../workers/lib/worker');
 
 let registry;
 
 const getRegistry = () => {
   if (!registry) {
-    registry = new Registry();
+    registry = new Worker('backend');
   }
   return registry;
 };
