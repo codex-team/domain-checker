339f3f53e5528a04f27b333135b7018b5231429d
Daniil Beltyukov <beltyukovdanil@gmail.com>
2018-12-01 21:16:07 +0300

    Add file documentation

--- backend/src/workers/zoneCheck/sortTlds.js
+++ backend/src/workers/zoneCheck/sortTlds.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+/**
+ * Simple script to sort tlds stores in json file as array by length and alphabetically
+ * 
+ * Example:
+ *  # ./sortTlds.js tlds.json
+ */
 
 const fs = require('fs');
 
