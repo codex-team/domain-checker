a0a4fc68089537cc98bc517349dca7b34cb41370
Nikita Melnikov <nikmel2803@gmail.com>
2018-12-03 14:54:19 +0300

    DNS worker check A and AAAA types

--- backend/src/workers/dns/checkDomain.js
+++ backend/src/workers/dns/checkDomain.js
@@ -8,14 +8,15 @@ const axios = require('axios');
  */
 
 /**
- * Checks if domain is available by DNS.
+ * Returns DNS status for searching domain
  * @param {string} name Base domain name w/o TLD.
  * @param {string} tld TLD.
- * @returns {boolean} true if available else false.
+ * @param {string} recordType - DNS record type
+ * @returns {Promise<number>}
  * @throws {Error} 'Invalid response from server',
  *                 'HTTPS request error'
  */
-const checkDomain = async (name, tld) => {
+const getDNSStatus = async (name, tld, recordType) => {
   /**
    * @const {string} Cloudflare DoH API endpoint
    */
@@ -32,19 +33,49 @@ const checkDomain = async (name, tld) => {
     /**
      * @type {string} url for sending DoH request
      */
-    const url = `${API_ENDPOINT}?name=${name}.${tld}&type=A`;
+    const url = `${API_ENDPOINT}?name=${name}.${tld}&type=${recordType}`;
 
     /**
      * @const {DoHResponse} response from server
      */
     const response = await axios.get(url, config);
 
-    if (response.data && response.data.Status) {
-      // Return false id domain available => can't buy domain
-      return !(response.data.Status === 0);
+    if ('data' in response && 'Status' in response.data) {
+      return response.data.Status;
     } else throw new Error('Invalid response from server');
   } catch (e) {
     console.log('HTTPS request error' + e);
+  }
+};
+
+/**
+ * Checks if domain is available by DNS.
+ * @param {string} name Base domain name w/o TLD.
+ * @param {string} tld TLD.
+ * @returns {boolean} true if available else false.
+ * @throws {Error} 'Invalid response from server',
+ *                 'HTTPS request error'
+ */
+const checkDomain = async (name, tld) => {
+  try {
+    /**
+     * @const {{A: string, AAAA: string}} checking DNS record types
+     */
+    const DNS_RECORD_TYPES = {
+      A: 'A',
+      AAAA: 'AAAA'
+    };
+
+    let dnsStatus = await getDNSStatus(name, tld, DNS_RECORD_TYPES.A);
+
+    if (dnsStatus === 0) {
+      return false;
+    }
+    dnsStatus = await getDNSStatus(name, tld, DNS_RECORD_TYPES.AAAA);
+    return !(dnsStatus === 0);
+  } catch (e) {
+    console.log('Error in DoH request' + e);
+    throw e;
   }
 };
 
